cc8802c8f9aa3fe3d693fef730ee9cdf0d9d7b4e
John-D-S <68535837+John-D-S@users.noreply.github.com>
2024-10-22 12:32:18 +1100

    made changes to token prefabs, teleporter and test scene

--- Assets/CharacterControllerRework/Scripts/Teleporter.cs
+++ Assets/CharacterControllerRework/Scripts/Teleporter.cs
@@ -35,8 +35,9 @@ namespace CharacterSystem
 
         private void OnDrawGizmos()
         {
+            Gizmos.color = new Color(0, 1, 0, 0.5f);
+            Gizmos.DrawSphere(transform.position, 1);
             Gizmos.color = new Color(1, 0, 0, 0.5f);
-            
             switch (Mode)
             {
                 case DestMode.ToPosition:
